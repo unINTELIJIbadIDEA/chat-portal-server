0e89389297116ce8c9da5c5805371b2a28747f55
Margiorno <m.zawiski03@gmail.com>
2025-05-02 16:08:54 +0200

    small configuration

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,4 @@
 module com.project {
-    requires javafx.controls;
-    requires javafx.fxml;
     requires jdk.httpserver;
     requires com.google.gson;
     requires java.sql;
@@ -9,15 +7,11 @@ module com.project {
     requires java.desktop;
 
 
-    opens com.project to javafx.fxml;
     opens com.project.dao to com.google.gson;
-    exports com.project;
     opens com.project.models to com.google.gson;
     opens com.project.rest to com.google.gson;
     opens com.project.adapters to com.google.gson;
     opens com.project.server to com.google.gson;
-    exports com.project.controllers;
-    opens com.project.controllers to javafx.fxml;
     exports com.project.utils;
     opens com.project.utils to javafx.fxml;
     opens com.project.models.message to com.google.gson;
