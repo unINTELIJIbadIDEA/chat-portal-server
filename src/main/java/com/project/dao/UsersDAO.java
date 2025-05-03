dd3680b2ec018507d176ce95b12e98d60019199c
Margiorno <m.zawiski03@gmail.com>
2025-05-03 22:30:19 +0200

    rebuilt user crud

--- src/main/java/com/project/dao/UsersDAO.java
+++ src/main/java/com/project/dao/UsersDAO.java
@@ -1,95 +1,144 @@
 package com.project.dao;
 
-import com.google.gson.Gson;
 import com.project.models.User;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsersDAO {
-
     private final String dbURL;
-    private final String dbName;
+    private final String dbUser;
     private final String dbPassword;
     private Connection connection;
 
-    public UsersDAO(String dbURL, String dbName, String dbPassword) {
+    public UsersDAO(String dbURL, String dbUser, String dbPassword) {
         this.dbURL = dbURL;
-        this.dbName = dbName;
+        this.dbUser = dbUser;
         this.dbPassword = dbPassword;
     }
 
     public void connect() throws SQLException {
-        connection = DriverManager.getConnection(dbURL, dbName, dbPassword);
+        connection = DriverManager.getConnection(dbURL, dbUser, dbPassword);
     }
 
     public void close() throws SQLException {
-        connection.close();
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
     }
 
-    public boolean addUser(User newUser) {
-        String query = "INSERT INTO user (name, surname, nickname, email, birthday, password) VALUES (?, ?, ?, ?, ?, ?)";
-        try (PreparedStatement statement = connection.prepareStatement(query)){
-
-            statement.setString(1, newUser.getName());
-            statement.setString(2, newUser.getSurname());
-            statement.setString(3, newUser.getNickname());
-            statement.setString(4, newUser.getEmail());
-            statement.setString(5, newUser.getBirthday());
-            statement.setString(6, newUser.getPassword());
-            return statement.executeUpdate() > 0;
-
+    public boolean addUser(User user) {
+        String sql = "INSERT INTO `user` (name, surname, nickname, email, birthday, password) VALUES (?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setString(1, user.getName());
+            stmt.setString(2, user.getSurname());
+            stmt.setString(3, user.getNickname());
+            stmt.setString(4, user.getEmail());
+            stmt.setDate(5, Date.valueOf(user.getBirthday())); // zakładamy format YYYY-MM-DD
+            stmt.setString(6, user.getPassword());
+            int affected = stmt.executeUpdate();
+            if (affected > 0) {
+                try (ResultSet keys = stmt.getGeneratedKeys()) {
+                    if (keys.next()) {
+                        user.setId(keys.getInt(1));
+                    }
+                }
+                return true;
+            }
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.err.println("addUser error: " + e.getMessage());
+        }
+        return false;
+    }
+
+    public boolean updateUser(User user) {
+        String sql = "UPDATE `user` SET name = ?, surname = ?, nickname = ?, email = ?, birthday = ?, password = ? WHERE userId = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, user.getName());
+            stmt.setString(2, user.getSurname());
+            stmt.setString(3, user.getNickname());
+            stmt.setString(4, user.getEmail());
+            stmt.setDate(5, Date.valueOf(user.getBirthday()));
+            stmt.setString(6, user.getPassword());
+            stmt.setInt(7, user.getId());
+            return stmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            System.err.println("updateUser error: " + e.getMessage());
             return false;
         }
     }
 
-    public String getUserWithId(int userId) {
-        String query = "SELECT * FROM user WHERE userId = ?";
-        try (PreparedStatement statement = connection.prepareStatement(query)){;
-            statement.setInt(1, userId);
-
-            ResultSet resultSet = statement.executeQuery();
-            User user = null;
-            while (resultSet.next()) {
-                user = new User(
-                        resultSet.getInt("userId"),
-                        resultSet.getString("name"),
-                        resultSet.getString("surname"),
-                        resultSet.getString("nickname"),
-                        resultSet.getString("email"),
-                        resultSet.getString("birthday"),
-                        resultSet.getString("password")
-                );
-            }
-
-            Gson gson = new Gson();
-            return gson.toJson(user);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return "";
+    public boolean deleteUser(int userId) {
+        String sql = "DELETE FROM `user` WHERE userId = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, userId);
+            return stmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            System.err.println("deleteUser error: " + e.getMessage());
+            return false;
         }
     }
 
-    public User getUserByEmail(String email) throws SQLException {
-        String query = "SELECT * FROM user WHERE email = ?";
-        try (PreparedStatement ps = connection.prepareStatement(query)) {
-            ps.setString(1, email);
-            ResultSet rs = ps.executeQuery();
-
-            if (rs.next()) {
-                int userId = rs.getInt("userId");
-                String name = rs.getString("name");
-                String surname = rs.getString("surname");
-                String nickname = rs.getString("nickname");
-                String emailFromDb = rs.getString("email");
-                String password = rs.getString("password");
-                String birthday = rs.getString("birthday");
-
-                return new User(userId, name, surname, nickname, emailFromDb, birthday, password);
+    public User getUserWithId(int userId) throws SQLException {
+        String sql = "SELECT * FROM `user` WHERE userId = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, userId);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return new User(
+                            rs.getInt("userId"),
+                            rs.getString("name"),
+                            rs.getString("surname"),
+                            rs.getString("nickname"),
+                            rs.getString("email"),
+                            rs.getDate("birthday").toString(),
+                            rs.getString("password")
+                    );
+                }
             }
         }
         return null;
     }
 
+    public User getUserByEmail(String email) throws SQLException {
+        String sql = "SELECT * FROM `user` WHERE email = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, email);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return new User(
+                            rs.getInt("userId"),
+                            rs.getString("name"),
+                            rs.getString("surname"),
+                            rs.getString("nickname"),
+                            rs.getString("email"),
+                            rs.getDate("birthday").toString(),
+                            rs.getString("password")
+                    );
+                }
+            }
+        }
+        return null;
+    }
+
+    public List<User> getAllUsers() throws SQLException {
+        String sql = "SELECT * FROM `user`";
+        List<User> list = new ArrayList<>();
+        try (Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                list.add(new User(
+                        rs.getInt("userId"),
+                        rs.getString("name"),
+                        rs.getString("surname"),
+                        rs.getString("nickname"),
+                        rs.getString("email"),
+                        rs.getDate("birthday").toString(),
+                        rs.getString("password")
+                ));
+            }
+        }
+        return list;
+    }
 }
--- src/main/java/com/project/rest/UsersHandler.java
+++ src/main/java/com/project/rest/UsersHandler.java
@@ -1,96 +1,114 @@
 package com.project.rest;
 
-//import com.project.dao.DatabaseConnection;
-import com.project.utils.Config;
-import com.project.dao.UsersDAO;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParseException;
+import com.project.adapters.UserAdapter;
 import com.project.models.User;
+import com.project.services.UsersService;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static com.project.server.ApiServer.*;
+import java.util.List;
 
 public class UsersHandler implements HttpHandler {
+    private final Gson gson = new GsonBuilder()
+            .registerTypeAdapter(User.class, new UserAdapter())
+            .create();
+    private final UsersService service = new UsersService();
 
     @Override
-    public void handle(HttpExchange exchange) {
-        String method = exchange.getRequestMethod();
+    public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod().toUpperCase();
+        String path = exchange.getHttpContext().getPath();
+        String rel = exchange.getRequestURI().getPath().substring(path.length());
+
         try {
-            if (method.equalsIgnoreCase("POST")) {
-                handlePostRequest(exchange);
-            } else if (method.equalsIgnoreCase("GET")) {
-                handleGetRequest(exchange);
-            }
-        } catch (IOException | SQLException e) {
-            System.out.println(e.getMessage());
-        }
-
-    }
-
-    private void handleGetRequest(HttpExchange exchange) throws SQLException, IOException {
-        Map<String, String> queryParams = getQueryParams(exchange.getRequestURI().getQuery());
-        String userId = queryParams.get("userId");
-        if (userId != null) {
-            UsersDAO dao = new UsersDAO(Config.getDbUrl(), Config.getDbUsername(), Config.getDbPassword());
-            dao.connect();
-            String user = dao.getUserWithId(Integer.parseInt(userId));
-            if (user.isEmpty()) {
-                sendResponse(exchange, 500, "Failed to find user with specific id");
+            if (rel.isEmpty() || rel.equals("/")) {
+                switch (method) {
+                    case "GET": handleGetAll(exchange); break;
+                    case "POST": handleAdd(exchange); break;
+                    default: exchange.sendResponseHeaders(405, -1);
+                }
             } else {
-                sendResponse(exchange, 200, user);
+                int id = Integer.parseInt(rel.substring(1));
+                switch (method) {
+                    case "GET": handleGetById(exchange, id); break;
+                    case "PUT": handleUpdate(exchange, id); break;
+                    case "DELETE": handleDelete(exchange, id); break;
+                    default: exchange.sendResponseHeaders(405, -1);
+                }
             }
-
-            dao.close();
+        } catch (NumberFormatException e) {
+            exchange.sendResponseHeaders(400, -1);
         }
-
     }
 
-    public Map<String, String> getQueryParams(String requestParams) {
-        if (requestParams == null) {
-            return Map.of();
+    private void handleGetAll(HttpExchange ex) throws IOException {
+        try {
+            List<User> users = service.getAllUsers();
+            sendJson(ex, 200, gson.toJson(users));
+        } catch (SQLException e) {
+            sendJson(ex, 500, "{ \"error\": \"Server error\" }");
         }
-
-        return Stream.of(requestParams.split("&"))
-                .map(p -> p.split("="))
-                .collect(Collectors.toMap(pp -> pp[0], pp -> pp[1]));
     }
 
-    private void handlePostRequest(HttpExchange exchange) throws IOException, SQLException {
-        InputStreamReader reader = new InputStreamReader(exchange.getRequestBody(), StandardCharsets.UTF_8);
-        BufferedReader bufferedReader = new BufferedReader(reader);
-
-        String requestBody = bufferedReader.lines().collect(Collectors.joining("\n"));
-        User newUser = gson.fromJson(requestBody, User.class);
-
-
-        UsersDAO dao = new UsersDAO(Config.getDbUrl(), Config.getDbUsername(), Config.getDbPassword());
-        dao.connect();
-        boolean isAdded = dao.addUser(newUser);
-
-        if (isAdded) {
-            sendResponse(exchange, 201, "User added successfully");
-        } else {
-            sendResponse(exchange, 500, "Failed to add user");
+    private void handleGetById(HttpExchange ex, int id) throws IOException {
+        try {
+            User user = service.getUserById(id);
+            if (user == null) ex.sendResponseHeaders(404, -1);
+            else sendJson(ex, 200, gson.toJson(user));
+        } catch (SQLException e) {
+            sendJson(ex, 500, "{ \"error\": \"Server error\" }");
         }
-
-        dao.close();
-        reader.close();
     }
 
-    private void sendResponse(HttpExchange exchange, int statusCode, String responseContent) throws IOException {
-        exchange.sendResponseHeaders(statusCode, responseContent.getBytes(StandardCharsets.UTF_8).length);
-        OutputStream os = exchange.getResponseBody();
-        os.write(responseContent.getBytes());
-        os.close();
+    private void handleAdd(HttpExchange ex) throws IOException {
+        InputStreamReader isr = new InputStreamReader(ex.getRequestBody(), StandardCharsets.UTF_8);
+        try {
+            User user = gson.fromJson(isr, User.class);
+            boolean ok = service.addUser(user);
+            if (ok) sendJson(ex, 201, gson.toJson(user));
+            else sendJson(ex, 400, "{ \"error\": \"Invalid data\" }");
+        } catch (JsonParseException | SQLException e) {
+            sendJson(ex, 400, "{ \"error\": \"Bad request\" }");
+        }
+    }
+
+    private void handleUpdate(HttpExchange ex, int id) throws IOException {
+        InputStreamReader isr = new InputStreamReader(ex.getRequestBody(), StandardCharsets.UTF_8);
+        try {
+            User user = gson.fromJson(isr, User.class);
+            user.setId(id);
+            boolean ok = service.updateUser(user);
+            if (ok) sendJson(ex, 200, "{ \"message\": \"Updated\" }");
+            else sendJson(ex, 400, "{ \"error\": \"Could not update\" }");
+        } catch (JsonParseException | SQLException e) {
+            sendJson(ex, 400, "{ \"error\": \"Bad request\" }");
+        }
+    }
+
+    private void handleDelete(HttpExchange ex, int id) throws IOException {
+        try {
+            boolean ok = service.deleteUser(id);
+            if (ok) sendJson(ex, 200, "{ \"message\": \"Deleted\" }");
+            else ex.sendResponseHeaders(404, -1);
+        } catch (SQLException e) {
+            sendJson(ex, 500, "{ \"error\": \"Server error\" }");
+        }
+    }
+
+    private void sendJson(HttpExchange ex, int status, String body) throws IOException {
+        byte[] bytes = body.getBytes(StandardCharsets.UTF_8);
+        ex.getResponseHeaders().set("Content-Type", "application/json; charset=UTF-8");
+        ex.sendResponseHeaders(status, bytes.length);
+        try (OutputStream os = ex.getResponseBody()) {
+            os.write(bytes);
+        }
     }
 }
-
